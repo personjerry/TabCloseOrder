42571246eb9b08bdd3079ee8a02fd1aa8c184857
Jerry Liu <jerry.liu@futureadvisor.com>
2014-07-23 13:22:56 -0700

    test commit 5

--- TabCloseOrder.py
+++ TabCloseOrder.py
@@ -3,25 +3,25 @@ import sublime_plugin
 
 class TabCloseOrder(sublime_plugin.EventListener):
     def on_load(self, view):
-        print (view.fileName() + "just got loaded")
+        print (view.file_name() + "just got loaded")
 
     def on_pre_save(self, view):
-        print (view.fileName(), "is about to be saved")
+        print (view.file_name(), "is about to be saved")
 
     def on_post_save(self, view):
-        print (view.fileName(), "just got saved")
+        print (view.file_name(), "just got saved")
         
     def on_new(self, view):
         print ("new file")
 
     def on_modified(self, view):
-        print (view.fileName(), "modified")
+        print (view.file_name(), "modified")
 
     def on_activated(self, view):
-        print (view.fileName(), "is now the active view")
+        print (view.file_name(), "is now the active view")
 
     def on_close(self, view):
-        print (view.fileName(), "is no more")
+        print (view.file_name(), "is no more")
 
     def on_clone(self, view):
-        print (view.fileName(), "just got cloned")
+        print (view.file_name(), "just got cloned")

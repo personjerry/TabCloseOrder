49a976a5b6d34f380365ea79de2aba34793b0649
Jerry Liu <jerry.liu@futureadvisor.com>
2014-07-23 14:49:08 -0700

    added self.

--- TabCloseOrder.py
+++ TabCloseOrder.py
@@ -10,7 +10,7 @@ class TabCloseOrder(sublime_plugin.EventListener):
     def on_activated(self, view):
         self.activewindow = view.window()
         if self.activewindow.id() not in self.viewlist:
-            self.viewlist[self.activewindow.id()] = list(map(operator.methodcaller('id'), activewindow.views()))
+            self.viewlist[self.activewindow.id()] = list(map(operator.methodcaller('id'), self.activewindow.views()))
 
         if view in self.viewlist[self.activewindow.id()]:
             self.viewlist[self.activewindow.id()].remove(view)

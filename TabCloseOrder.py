9183ecd9a371b5f1746edacf7fa6733ca680b9c3
Jerry Liu <jerry.liu@futureadvisor.com>
2014-07-23 16:44:25 -0700

    changed focus view call to id

--- TabCloseOrder.py
+++ TabCloseOrder.py
@@ -6,30 +6,30 @@ class TabCloseOrder(sublime_plugin.EventListener):
     def __init__(self):
         self.viewlist = {}
         self.activewindow = None
-        print ("initialized")
+        # print ("initialized")
 
     def on_activated(self, view):
-        print ("new window activated")
-        print (self.viewlist)
+        # print ("new window activated")
+        # print (self.viewlist)
         self.activewindow = view.window()
         if self.activewindow.id() not in self.viewlist:
-            print ("window not in repository, adding")
+            # print ("window not in repository, adding")
             self.viewlist[self.activewindow.id()] = self.activewindow.views()
-            print (self.viewlist)
+            # print (self.viewlist)
 
         if view in self.viewlist[self.activewindow.id()]:
-            print ("removed self from viewlist")
+            # print ("removed self from viewlist")
             self.viewlist[self.activewindow.id()].remove(view)
-            print (self.viewlist)
+            # print (self.viewlist)
 
-        print ("adding self to viewlist")
+        # print ("adding self to viewlist")
         self.viewlist[self.activewindow.id()].append(view)
-        print (self.viewlist)
+        # print (self.viewlist)
 
     def on_close(self, view):
-        print ("closed view")
-        print (self.viewlist)
+        # print ("closed view")
+        # print (self.viewlist)
         if view in self.viewlist[self.activewindow.id()]:
             self.viewlist[self.activewindow.id()].remove(view)
-        print (view.window())
-        self.activewindow.focus_view(self.viewlist[self.activewindow.id()][-1])
+        # print (view.window())
+        self.activewindow.focus_view(self.viewlist[self.activewindow.id()][-1].id())

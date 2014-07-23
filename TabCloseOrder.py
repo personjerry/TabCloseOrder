4eab5fa97eb665b50e35a81daf4be7c0c147ec52
Jerry Liu <jerry.liu@futureadvisor.com>
2014-07-23 14:28:02 -0700

    replaced tabs with spaces

--- TabCloseOrder.py
+++ TabCloseOrder.py
@@ -2,21 +2,21 @@ import sublime
 import sublime_plugin
 
 class TabCloseOrder(sublime_plugin.EventListener):
-	def __init__(self):
-		self.viewlist = {}
+    def __init__(self):
+        self.viewlist = {}
 
     def on_activated(self, view):
-    	window = view.window().id()
+        window = view.window().id()
         if window not in self.viewlist:
-        	self.viewlist[window] = view.window().views().map(operator.methodcaller('id'))
+            self.viewlist[window] = view.window().views().map(operator.methodcaller('id'))
 
         if view in self.viewlist[window]:
-        	self.viewlist[window].remove(view)
+            self.viewlist[window].remove(view)
 
-    	self.viewlist[window].append(view)
+        self.viewlist[window].append(view)
 
     def on_close(self, view):
-    	window = view.window().id()
+        window = view.window().id()
         if view in self.viewlist[window]:
-        	self.viewlist[window].remove(view)
-    	view.window().focus_view(self.viewlist[window][-1])
+            self.viewlist[window].remove(view)
+        view.window().focus_view(self.viewlist[window][-1])

d682b9e5d8c2bd43745d7b7417a9b64da29e506d
Jerry Liu <jerry.liu@futureadvisor.com>
2014-07-23 17:05:15 -0700

    trying new method

--- TabCloseOrder.py
+++ TabCloseOrder.py
@@ -8,18 +8,17 @@ class TabCloseOrder(sublime_plugin.EventListener):
 
     def on_activated(self, view):
         window = view.window()
-        if window not in self.viewlist:
-            self.viewlist[window] = self.activewindow.views()
+        if window.id() not in self.viewlist:
+            self.viewlist[window.id()] = (window, self.activewindow.views())
 
-        if view in self.viewlist[window]:
-            self.viewlist[window].remove(view)
+        if view in self.viewlist[window.id()][1]:
+            self.viewlist[window.id()][1].remove(view)
 
-        self.viewlist[window].append(view)
+        self.viewlist[window][1].append(view)
 
     def on_closed(self, view):
-        for window in self.viewlist.keys:
-            if view in self.viewlist[window]:
-                self.viewlist[window].remove(view)
-                print ("Trying to focus on view " + self.viewlist[window][-1] + " from window " + window)
-                window.focus_view(self.viewlist[window][-1])
+        for window_id in self.viewlist.keys:
+            if view in self.viewlist[window_id][1]:
+                self.viewlist[window_id][1].remove(view)
+                self.viewlist[window_id][0]focus_view(self.viewlist[window_id][1][-1])
                 return

3fdbfb2eb69f740af4919cbfa26619a64f8d2274
Jerry Liu <jerry.liu@futureadvisor.com>
2014-07-23 18:50:06 -0700

    using logic through sublime windows

--- TabCloseOrder.py
+++ TabCloseOrder.py
@@ -4,26 +4,29 @@ import operator
 
 class TabCloseOrder(sublime_plugin.EventListener):
     def __init__(self):
-        self.viewlist = {}
+        self.view_list = {}
 
     def on_activated(self, view):
-        window = view.window().id()
-        if window not in self.viewlist:
-            self.viewlist[window] = list(map(operator.methodcaller('id'), view.window().views()))
+        window_id = view.window().id()
+        if window_id not in self.view_list:
+            self.view_list[window_id] = list(map(operator.methodcaller('id'), view.window().views()))
 
-        if view.id() in self.viewlist[window]:
-            self.viewlist[window].remove(view.id())
+        if view.id() in self.view_list[window_id]:
+            self.view_list[window_id].remove(view.id())
 
-        self.viewlist[window].append(view.id())
+        self.view_list[window_id].append(view.id())
 
     def on_close(self, view):
-        for window in self.viewlist:
-            if view.id() in self.viewlist[window]:
-                self.viewlist[window].remove(view.id())
-        for window in sublime.windows():
-            if window.get_view_index(view.id()) != -1:
-                result = window.get_view_index(self.viewlist[window.id()][-1])
-                window.focus_view(result[0][result[1]])
+        for window_id in self.view_list:
+            if view.id() in self.view_list[window_id]:
+                self.view_list[window_id].remove(view.id())
+                sublime_window = None
+                for window in sublime.windows():
+                    if window.id() == window_id:
+                        sublime_window = window
+                result = sublime_window.get_view_index(self.view_list[window_id][-1])
+                print ("attempting window ", sublime_window.id(), " with ", result[0][result[1]].id())
+                sublime_window.focus_view(result[0][result[1]])
 
 
 

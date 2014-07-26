3740349c20a64d0b1baf7c0993b8bb803bdde64e
Jerry Liu <jerry.liu@futureadvisor.com>
2014-07-25 18:24:04 -0700

    now using deepcopy

--- TabCloseOrder.py
+++ TabCloseOrder.py
@@ -1,12 +1,15 @@
 import sublime
 import sublime_plugin
 import operator
+import copy
 
 class TabCloseOrder(sublime_plugin.EventListener):
     def __init__(self):
         self.view_list = {}
+        self.backup_list = {}
 
     def on_activated(self, view):
+        self.backup_list = copy.deepcopy(self.view_list)
         window_id = view.window().id()
         if window_id not in self.view_list:
             self.view_list[window_id] = list(map(operator.methodcaller('id'), view.window().views()))
@@ -19,6 +22,7 @@ class TabCloseOrder(sublime_plugin.EventListener):
             print (self.view_list[key])
 
     def on_close(self, view):
+        self.view_list = self.backup_list
         for window_id in self.view_list:
             if view.id() in self.view_list[window_id]:
                 self.view_list[window_id].remove(view.id())
